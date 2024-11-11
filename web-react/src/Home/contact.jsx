533602aeff2f40ed434d3bd7e088806c6e74cd89
Patel Mark <patelmark5330@gmail.com>
2024-11-11 16:10:13 +0530

    email send complete

--- web-react/src/Home/contact.jsx
+++ web-react/src/Home/contact.jsx
@@ -1,6 +1,34 @@
 import React from "react";
+import Swal from 'sweetalert2'
 
 const Contact = () => {
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "7a0f3de7-62e8-4426-a00a-e5c609fbba9d");
+
+    const object = Object.fromEntries(formData);
+    const json = JSON.stringify(object);
+
+    const res = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: json,
+    }).then((res) => res.json());
+
+    if (res.success) {
+      Swal.fire({
+        title: "Success!",
+        text: "Message sent successfully",
+        icon: "success"
+      });
+    }
+  };
+
   return (
     <>
       <section className="contact section" id="contact">
@@ -41,22 +69,18 @@ const Contact = () => {
                 <br />
                 <span>design work or partnerships.</span>
               </p>
-              <form
-                onSubmit="sendEmail(); reset(); return false;"
-                action=""
-                className="contact-form"
-              >
+              <form action="" className="contact-form" onSubmit={onSubmit}>
                 <div className="first-row">
-                  <input type="text" placeholder="Name" id="name" />
+                  <input type="text" placeholder="Name" name="Name" required />
                 </div>
                 <div className="second-row">
-                  <input type="email" placeholder="Email" id="email" />
-                  <input type="text" placeholder="Subject" id="subject" />
+                  <input type="email" placeholder="Email" name="Email" required />
+                  <input type="text" placeholder="Subject" name="Subject" required />
                 </div>
                 <div className="third-row">
                   <textarea
-                    name=""
-                    id="message"
+                    name="Message"
+                    id=""
                     rows={7}
                     placeholder="Message"
                     defaultValue={""}

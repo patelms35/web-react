32e8808e17cf7240f108941a341411d78ae81a02
Patel Mark <patelmark5330@gmail.com>
2024-11-11 20:43:50 +0530

    vercel/speed-insights

--- web-react/src/App.jsx
+++ web-react/src/App.jsx
@@ -2,6 +2,7 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Home from "./Home/home";
 import Portfolio from "./Portfolio/portfolio";
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Route path="/portfolio" element={<Portfolio />} />
       </Routes>
       <Analytics />
+      <SpeedInsights />
     </Router>
   );
 };
